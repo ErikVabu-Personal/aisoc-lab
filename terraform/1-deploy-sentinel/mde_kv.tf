5a5758749902a8dba604609fe5117f18b6ab8fb8
Erik Van Buggenhout <erik@users.noreply.github.com>
2026-04-04 17:18:04 +0000

    Grant operator Key Vault secret Recover permission for soft-delete scenarios

--- terraform/1-deploy-sentinel/mde_kv.tf
+++ terraform/1-deploy-sentinel/mde_kv.tf
@@ -36,7 +36,7 @@ resource "azurerm_key_vault" "mde" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
-    secret_permissions = ["Get", "List", "Set", "Delete"]
+    secret_permissions = ["Get", "List", "Set", "Delete", "Recover"]
   }
 
   # NOTE: Terraform requires lifecycle.prevent_destroy to be a constant.
