af95eeaa39862d3b46b2bb34c144ae513f94205d
Erik Van Buggenhout <erik@users.noreply.github.com>
2026-04-23 12:14:51 +0000

    terraform: disable Key Vault purge on destroy (tenant policy friendly)

--- terraform/1-deploy-sentinel/providers.tf
+++ terraform/1-deploy-sentinel/providers.tf
@@ -27,7 +27,10 @@ provider "azurerm" {
     key_vault {
       # Some tenants enforce policies that forbid purging secrets/vaults.
       # Disable purge-on-destroy so `terraform destroy` can succeed without requiring purge permissions.
-      purge_soft_delete_on_destroy = false
+      purge_soft_deleted_secrets_on_destroy      = false
+      purge_soft_deleted_keys_on_destroy         = false
+      purge_soft_deleted_certificates_on_destroy = false
+
       recover_soft_deleted_key_vaults = true
     }
   }
